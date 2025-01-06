b8912ef8e4c79deb0916889b9d2fa9b6c4fb80e5
Throk-117 <193827398+Throk-117@users.noreply.github.com>
2025-01-06 04:35:19 -0500

    comments.js

--- comments.js
+++ comments.js
@@ -1,1 +1,14 @@
 // Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
